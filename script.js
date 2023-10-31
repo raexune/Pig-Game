37565da3e86efdc8229c8c1f123db15f219d43d4
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2023-10-31 20:28:50 +0100

    set back to 100

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ btnHold.addEventListener('click', function(){
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
         //2.Check if players score is >= 100
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= 100){
         playing = false;
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
